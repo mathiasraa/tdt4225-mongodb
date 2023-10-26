fdb3bc7dd390b92321761f6dd6be08aa0e1d4f58
Mathias Raa <skraagen@hotmail.com>
2023-10-26 22:41:43 +0200

    latest & greatest

--- src/migrate.py
+++ src/migrate.py
@@ -1,5 +1,6 @@
 from bson import ObjectId
 from pymongo import MongoClient
+from pymongo.database import Database
 from tqdm import tqdm
 from utils.data import (
     get_trajectories_df,
@@ -9,7 +10,6 @@ from utils.data import (
 )
 from utils.connection import DbConnector
 from datetime import datetime
-from pymongo.database import Database
 
 
 def create_user_collection(client: MongoClient, db: Database):
@@ -60,21 +60,6 @@ def create_activity_collection(client: MongoClient, db: Database):
 
     collection = db.create_collection(collection_name)
 
-    # query = f"""
-    # CREATE TABLE IF NOT EXISTS {table_name} (
-    #         id INT AUTO_INCREMENT NOT NULL,
-    #         user_id VARCHAR(255) NOT NULL,
-    #         start_date_time DATETIME,
-    #         end_date_time DATETIME,
-    #         transportation_mode VARCHAR(20),
-    #         FOREIGN KEY (user_id) REFERENCES UserTable(id),
-    #         PRIMARY KEY (id)
-    #     )
-    # """
-
-    # cursor.execute(query)
-    # db_connection.commit()
-
     # Insert data
     user_ids = get_user_ids()
 
@@ -90,6 +75,9 @@ def create_activity_collection(client: MongoClient, db: Database):
         trajectories_df = get_trajectories_df(user)
 
         for _, row in activities_df.iterrows():
+            if trajectories_df[trajectories_df["activity_id"] == row["id"]].empty:
+                continue
+
             entry = {
                 "_id": row["id"],
                 "user": row["user_id"],
@@ -102,6 +90,7 @@ def create_activity_collection(client: MongoClient, db: Database):
                     "records"
                 ),
             }
+
             docs.append(entry)
 
         if len(docs) == 0:
@@ -111,137 +100,6 @@ def create_activity_collection(client: MongoClient, db: Database):
         collection.insert_many(docs)
 
     print("Created collection: \n", collection)
-
-
-# def create_track_point_table(client: MongoClient, db: Database):
-#     """
-#     Creates 'TrackPointTable' SQL table with the following columns:
-#     - 'id' (INT AUTO_INCREMENT NOT NULL PRIMARY KEY)
-#     - 'activity_id' (INT NOT NULL FOREIGN KEY)
-#     - 'lat' (FLOAT)
-#     - 'lon' (FLOAT)
-#     - 'altitude' (FLOAT)
-#     - 'date_time' (DATETIME)
-#     """
-
-#     # Create table
-#     table_name = "TrackPointTable"
-
-#     query = f"""
-#     CREATE TABLE IF NOT EXISTS {table_name} (
-#             id INT AUTO_INCREMENT NOT NULL,
-#             activity_id INT NOT NULL,
-#             lat FLOAT,
-#             lon FLOAT,
-#             altitude FLOAT,
-#             date_time DATETIME,
-#             FOREIGN KEY (activity_id) REFERENCES ActivityTable(id),
-#             PRIMARY KEY (id)
-#         )
-#     """
-
-#     cursor.execute(query)
-#     db_connection.commit()
-
-#     # Insert data
-#     user_ids = get_user_ids()
-
-#     pbar = tqdm(user_ids)
-#     for user in pbar:
-#         pbar.set_description("Processing TrackPoints of user %s" % user)
-#         trajectories_df = get_trajectories_df(user)
-#         data_to_insert = []
-
-#         pbar.set_description("Appending TrackPoints of user %s" % user)
-#         for _, row in trajectories_df.iterrows():
-#             data_to_insert.append(
-#                 (
-#                     row["activity_id"],
-#                     row["latitude"],
-#                     row["longitude"],
-#                     row["altitude"],
-#                     row["date_time"].strftime("%Y-%m-%d %H:%M:%S"),
-#                 )
-#             )
-
-#         pbar.set_description("Migrating TrackPoints of user %s" % user)
-
-#         insert_query = f"""
-#             INSERT INTO {table_name} (activity_id, lat, lon, altitude, date_time)
-#             VALUES (%s, %s, %s, %s, %s)
-#         """
-#         cursor.executemany(insert_query, data_to_insert)
-#         db_connection.commit()
-
-
-# def create_track_point_table_spatial(client: MongoClient, db: Database):
-#     """
-#     Creates 'TrackPointTable' SQL table with the following columns:
-#     - 'id' (INT AUTO_INCREMENT NOT NULL PRIMARY KEY)
-#     - 'activity_id' (INT NOT NULL FOREIGN KEY)
-#     - 'lat' (FLOAT)
-#     - 'lon' (FLOAT)
-#     - 'altitude' (FLOAT)
-#     - 'date_time' (DATETIME)
-#     - 'geom_point' (POINT, a spatial column to store the lon, lat values)
-#     """
-
-#     # Create table
-#     table_name = "TrackPointTable"
-
-#     query = f"""
-#     CREATE TABLE IF NOT EXISTS {table_name} (
-#             id INT AUTO_INCREMENT NOT NULL,
-#             activity_id INT NOT NULL,
-#             lat FLOAT,
-#             lon FLOAT,
-#             altitude FLOAT,
-#             date_time DATETIME,
-#             geom_point POINT NOT NULL,
-#             FOREIGN KEY (activity_id) REFERENCES ActivityTable(id),
-#             PRIMARY KEY (id)
-#         )
-#     """
-
-#     cursor.execute(query)
-
-#     # Add spatial index
-#     index_query = f"ALTER TABLE {table_name} ADD SPATIAL INDEX(geom_point)"
-#     cursor.execute(index_query)
-
-#     db_connection.commit()
-
-#     # Insert data
-#     user_ids = get_user_ids()
-
-#     pbar = tqdm(user_ids)
-#     for user in pbar:
-#         pbar.set_description("Processing TrackPoints of user %s" % user)
-#         trajectories_df = get_trajectories_df(user)
-#         data_to_insert = []
-
-#         pbar.set_description("Appending TrackPoints of user %s" % user)
-#         for _, row in trajectories_df.iterrows():
-#             data_to_insert.append(
-#                 (
-#                     row["activity_id"],
-#                     row["latitude"],
-#                     row["longitude"],
-#                     row["altitude"],
-#                     row["date_time"],
-#                     row["longitude"],
-#                     row["latitude"],
-#                 )
-#             )
-
-#         pbar.set_description("Migrating TrackPoints of user %s" % user)
-
-#         insert_query = f"""
-#             INSERT INTO {table_name} (activity_id, lat, lon, altitude, date_time, geom_point)
-#             VALUES (%s, %s, %s, %s, %s, POINT(%s, %s))
-#         """
-#         cursor.executemany(insert_query, data_to_insert)
-#         db_connection.commit()
 
 
 def drop_tables(client: MongoClient, db: Database):
